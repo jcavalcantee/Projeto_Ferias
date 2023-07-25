78b37ac448dffdd98996cfa5e488478a9eb9bf9a
jcavalcantee <jcavalcantessilva@gmail.com>
2023-07-24 23:33:48 -0300

    Co-authored-by: Gabriel Souza Lomba <GabrielLomba12@users.noreply.github.com> Co-authored-by: LUCAS PEREIRA ALVES DE GOUVEIA <LucasGouveia02@users.noreply.github.com> Co-authored-by: Pedro Augusto <pedrootsugua@users.noreply.github.com> Co-authored-by: Patrick Santos <PatrickSantos17@users.noreply.github.com>

--- src/Carrinho/Carrinho.java
+++ src/Carrinho/Carrinho.java
@@ -1,5 +1,30 @@
 package Carrinho;
+import java.util.Scanner;
+import Pedidos.Pedidos;
+import Menu.MenuTeste;
 
 public class Carrinho {
-    
+  
+    static int count = 0;
+    static float total = 0;
+
+    public static void carrinho() {
+
+        MenuTeste menuTeste = new MenuTeste();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nVocê deseja continuar a compra?");
+        String r1 = sc.nextLine();
+        
+        if (r1.equalsIgnoreCase("S")){
+            total = Pedidos.getSubTotal() + total;
+            count++;
+            menuTeste.menuTeste();
+
+        } else if (r1.equalsIgnoreCase("N") && count == 0){
+            System.out.println("O valor total do seu pedido é: R$" + Pedidos.getSubTotal());
+        } else if (r1.equalsIgnoreCase("N") && count != 0){
+            total = Pedidos.getSubTotal() + total;
+            System.out.println("O valor total do seu pedido é: R$" + total);
+        }
+    }
 }
--- src/Menu/MenuInicial.java
+++ src/Menu/MenuInicial.java
@@ -1,5 +1,39 @@
 package Menu;
+import java.util.Scanner;
+import Menu.InfoMenu.Informacoes;
 
 public class MenuInicial {
+    Scanner sc = new Scanner(System.in);
+
+    public void menuPrincipal(){
+        boolean sair = false;
+        do {
+            System.out.println("\n--------------------------------------------------------------------------------------------\n");
+            System.out.println("|============================|");
+            System.out.println("|     LANCHONETE DOS CRIA    |");
+            System.out.println("\n|============================|");
+            System.out.println("|       MENU DE OPÇÕES       |");
+            System.out.println("|============================|");
+            System.out.println("|    1- Informações          |");
+            System.out.println("|    2- Cardápio             |");
+            System.out.println("|    3- Lançamentos          |");
+            System.out.println("|    4- Trabalhe conosco     |");
+            System.out.println("|    5- Sair                 |");
+            System.out.println("|============================|");
+            System.out.print("Digite sua opção: ");
+
+            int opcao = sc.nextInt();
+
+            switch (opcao) {
+			case 1:
+                Menu.InfoMenu.Informacoes();
+                break;
+            
+                
+
+        } while(sair == false);
+    }
+
     
 }
+
